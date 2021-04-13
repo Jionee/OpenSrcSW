2a7c0cfd879693d65f67ee699b7fe94480cc6f37
Jionee <dnjswldb98@naver.com>
2021-04-13 12:13:31 +0900

    Week6 - Merge with feature branch Try to merge with feature branch, conflict emerged. >> So change feature branch name getResultCalc function > getInnerproduct >> Checkout to master branch >> Add getInnerProduct function from feature branch, >> Add getResultCalc function originally from master branch.

--- src/searcher.java
+++ src/searcher.java
@@ -15,7 +15,13 @@ import java.util.HashMap;
 public class searcher {
     public void CalcSim(File path, String query) throws IOException, ClassNotFoundException {
         //쿼리 kkmastring 분석, index.post ==> 각 문서별 유사도 계산
-        ArrayList<Double> resultCalc = getInnerProduct(path, query);
+        HashMap<String, Double> queryMap = new HashMap<>(); //kkma 분석
+        HashMap hashMap = getQueryMap_hashMap(path, query, queryMap);
+
+        ArrayList<Double> resultInnerProduct = getInnerProduct(queryMap, hashMap); //feature
+        ArrayList<Double> resultCalc = getResultCalc(queryMap, hashMap,resultInnerProduct); //master
+        //System.out.println(queryMap);
+        //System.out.println(hashMap);
 
         //title가져오기
         File path2 = new File("src/xmlSet/index.xml");
@@ -38,13 +44,79 @@ public class searcher {
             System.out.println("Rank"+(i+1)+" : "+titleList.get(i));
         }
     }
+    private ArrayList<Double> getInnerProduct(HashMap<String, Double> queryMap, HashMap hashMap){
 
-    private ArrayList<Double> getInnerProduct(File path, String query) throws IOException, ClassNotFoundException {
+        HashMap<String, Double> calResult = new HashMap<>();
+
+        //내적
+        ArrayList<Double> resultInnerProduct = new ArrayList<>();
+        for(int i=0;i<5;i++){ //5개 문서에 대해서 유사도 내적 계산 (3번째 자리에서 반올림한 값으로)
+            Double result0 = 0.0;
+            Double val = 0.0;
+            for(String key:queryMap.keySet()){
+                ArrayList<String> value = (ArrayList<String>) hashMap.get(key);
+                //System.out.println("~~~~ "+value);
+                for(int k=0;k<value.size();){
+                    if(Integer.parseInt(value.get(k))==i){
+                        val = Double.parseDouble(value.get(k+1));
+                        //System.out.println(i+" && "+val);
+                        break;
+                    }
+                    k+=2;
+                }
+                result0 += queryMap.get(key) * val;
+            }
+            resultInnerProduct.add(Math.round(result0*100)/100.0);
+        }
+        //System.out.println(resultCalc);
+        return resultInnerProduct;
+    }
+
+    private ArrayList<Double> getResultCalc(HashMap<String, Double> queryMap, HashMap hashMap, ArrayList<Double> resultInnerProduct){
+
+        //내적 --> Cosine
+        ArrayList<Double> resultCalc = new ArrayList<>();
+        Double powKkma = 0.0;//query제곱
+        Double sqrtPowKkma = 0.0;
+        Double sqrt = 0.0;
+        for(String key:queryMap.keySet()){
+            powKkma += Math.pow(queryMap.get(key), 2);
+        }
+        sqrtPowKkma = Math.sqrt(powKkma);
+        for(int i=0;i<5;i++){ //5개 문서에 대해서 Cosine 계산 (3번째 자리에서 반올림한 값으로)
+            Double result0 = 0.0;
+            Double powPost = 0.0;//
+            Double val = 0.0;
+            for(String key:queryMap.keySet()){ //Query를 kkmaString 분석한 것
+                ArrayList<String> value = (ArrayList<String>) hashMap.get(key);
+                for(int k=0;k<value.size();) {
+                    if (Integer.parseInt(value.get(k)) == i) {
+                        //루트 wq1제곱 + wq2제곱 + ..
+                        val = Double.parseDouble(value.get(k + 1));
+                        powPost += Math.pow(val,2);//
+                        //System.out.println(i+" && "+val);
+                        break;
+                    }
+                    k += 2;
+                }
+            }
+            Double sqrtPowPost = Math.sqrt(powPost);//
+            sqrt = sqrtPowKkma * sqrtPowPost;
+            result0 = resultInnerProduct.get(i) / (sqrt);
+            
+            resultCalc.add(Math.round(result0*100)/100.0);
+        }
+        System.out.println("RESULTCALC : "+ resultCalc);
+
+        return resultCalc;
+    }
+
+    private HashMap getQueryMap_hashMap(File path, String query, HashMap<String, Double> queryMap) throws IOException, ClassNotFoundException {
         //kkmastring 분석
         KeywordExtractor ke = new KeywordExtractor();
         KeywordList kl = ke.extractKeyword(query,true); //extract keyword
         //System.out.println("extract ==> " + query);
-        HashMap<String, Double> queryMap = new HashMap<>();
+        //HashMap<String, Double> queryMap = new HashMap<>();
 
         for(int i=0;i<kl.size();i++){
             Keyword kwrd = kl.get(i);
@@ -60,39 +132,8 @@ public class searcher {
         objectInputStream.close();
 
         HashMap hashMap = (HashMap) object;
-        HashMap<String, Double> calResult = new HashMap<>();
-
-        //내적 --> Cosine
-        ArrayList<Double> resultCalc = new ArrayList<>();
-        for(int i=0;i<5;i++){ //5개 문서에 대해서 유사도 내적 --> Cosine 계산 (3번째 자리에서 반올림한 값으로)
-            Double result0 = 0.0;
-            Double powKkma = 0.0;
-            Double powPost = 0.0;
-            Double val = 0.0;
-            for(String key:queryMap.keySet()){ //Query를 kkmaString 분석한 것
-                ArrayList<String> value = (ArrayList<String>) hashMap.get(key);
-                //System.out.println("~~~~ "+value);
-                for(int k=0;k<value.size();) {
-                    if (Integer.parseInt(value.get(k)) == i) {
-                        //루트 wq1제곱 + wq2제곱 + ..
-                        val = Double.parseDouble(value.get(k + 1));
-                        powKkma += Math.pow(queryMap.get(key), 2);
-                        powPost += Math.pow(val,2);
-                        //System.out.println(i+" && "+val);
-                        break;
-                    }
-                    k += 2;
-                }
-                Double sqrt = Math.sqrt(powKkma) * Math.sqrt(powPost);
-                result0 += queryMap.get(key) * val / sqrt;
-            }
-            resultCalc.add(Math.round(result0*100)/100.0);
-        }
-        System.out.println(resultCalc);
-
-        return resultCalc;
+        return hashMap;
     }
-
 
     public static ArrayList<String> readTitle(File file) throws IOException {
         //Read file by String
